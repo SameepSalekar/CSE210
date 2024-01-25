aaea208ab8500375cc11a5ccbd199d5c2356787e
SameepSalekar <sameepsalekar749@gmail.com>
2024-01-24 18:21:50 -0600

    First exercise

--- C#Prep/C#Prep1.cs
+++ C#Prep/C#Prep1.cs
@@ -5,7 +5,13 @@ namespace game
     {
         static void Main(string[]args)
         {
-            Console.WriteLine("Hello Prep 1 World");
+            Console.WriteLine("What is your first name?");
+            string first_name = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            string last_name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
         }
     }
 }
